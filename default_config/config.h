6a694c004ec02b30c6abd8df67192cbda75d4d6c
Nathaniel Jackson <njackson@student.42Adel.org.au>
2024-07-18 18:05:39 +0930

    Removed config file, and put it in .gitignore

--- default_config/config.h
+++ default_config/config.h
@@ -6,13 +6,15 @@
 /*   By: njackson <njackson@student.42adel.org.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 17:43:49 by njackson          #+#    #+#             */
-/*   Updated: 2024/07/18 17:52:48 by njackson         ###   ########.fr       */
+/*   Updated: 2024/07/18 18:06:55 by njackson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONFIG_H
 # define CONFIG_H
 
+// TO CHANGE THIS, COPY IT INTO A NEW FOLDER AT THE ROOT OF THE REPOSITORY
+// CALLED "config"
 # define PS1 "minishell \e[38;2;0;100;100m%p\x1b[0m $> "
 
 #endif
