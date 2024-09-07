811ddfc56720eb0404e5143539230c6ab69fa168
Bethany Milford 42macs <bmilford@student.42adel.org.au>
2024-09-07 16:47:06 +0930

    haha rl didnt compile

--- src/ms_env.c
+++ src/ms_env.c
@@ -6,7 +6,7 @@
 /*   By: bmilford <bmilford@student.42adel.o>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 17:30:05 by bmilford          #+#    #+#             */
-/*   Updated: 2024/09/04 19:36:28 by njackson         ###   ########.fr       */
+/*   Updated: 2024/09/07 16:45:56 by bmilford         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,16 @@
 
 int	ms_env(char **argv)
 {
-	int		index;
-	int		count;
 	char	**env;
+	int		index;
 
 	(void)argv;
 	index = 0;
-	count = 0;
 	env = ft_full_env();
 	while (env[index] != NULL)
-		index++;
-	while (count <= index)
 	{
-		printf("%s\n", env[count]);
-		count++;
+		printf("%s\n", env[index]);
+		index++;
 	}
 	return (0);
 }
